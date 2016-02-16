635691f90bbdc880eecd862d5aa18971327ca5e4
blueved <vincent.ferre@blueved.com>
2016-02-16 17:23:22 -0600

    add login service request

--- js/app.js
+++ js/app.js
@@ -1,38 +1,48 @@
 (function() {
 	  var app = angular.module('securityApp', []);
-
-	  app.controller('PaperFormController', ['$http', function($http){
-		var store = this;
-		store.products = [];
-		/*
-		$http.get('./store-products.json').success(function(data){
-			store.products = data;
-		});
-		*/
-	  }]);
-
-	  app.controller('FormController', ['$http',function($http) {
-		var self = this;
-		var formData = {
+	  app.controller('MainController', function(){
+		  
+	  });
+	  app.controller ('LoginController', ['$http','requestService', function($http, requestService){
+		  var self = this;
+		  var formData = {
 			firstname: "default",
 			lastname:"default",
+			username:"default",
+			passwrd:"default",
 			emailaddress: "default",
 			textareacontent: "default",
 			gender: "default",
 			member: false,
 			file_profile: "default",
 			file_avatar: "default"
-		};
+			};
+			
+			self.save = function() {
+				formData = self.form;
+			};
 
-		self.save = function() {
-			formData = self.form;
-		};
+			self.submitForm = function() {        
+				formData = self.form;
+				console.log(formData);
+				requestService( data).success(function(){
+					console.log("success");
+				});
+			};
+			
+			self.init = function(){
+			};
 
-		self.submitForm = function() {        
-			formData = self.form;
-			console.log(formData);
-			console.log("posting data....", formData);
-			//$http.post('form.php', JSON.stringify(data)).success(function(){/*success callback*/});
-		};
-	}]);
+			self.init();
+	  }]);
+	  
+	  app.factory('requestService', function($http){
+		  var factory = {};
+		  factory.loginRequest = function(data){
+				var url = "logingRequest/data"; 
+				return $http({  method: 'GET',  url: '/url'});
+		  };
+		  
+		  return factory;
+	  });
   })();
